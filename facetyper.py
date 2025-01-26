8263330cf9476bc1e95d0b24a4b8142374726595
Stuart Walker <stuartw1@gmail.com>
2025-01-26 15:12:01 +0000

    input loop

--- facetyper.py
+++ facetyper.py
@@ -14,13 +14,17 @@ device = USBKeyboardDevice()
 
 async def type_letters():
 
-    print(2)
     # Wait for device to connect
     await asyncio.sleep(2)
-    print(3)
-    # Type a string with the device
-    await device.type_string("echo hello, facedancer\n")
-    print(4)
+    #Input loop
+    print("Entering loop. Enter to send. Ctrl+C to exit")#
+    while True:
+        sendstring = ""
+        sendstring = input("Type string to send: ")
+        assert type(sendstring) == str, "input should be a String"
+        # Type a string with the device
+        await device.type_string(sendstring)
+    print("Sent: "+sendstring)
 
 print(1)
 main(device, type_letters())
